a01d972400096b497d448b59076dc6d3174a6a38
yuhdc <74478717+yuhdc@users.noreply.github.com>
2020-11-29 13:41:04 +0700

    version1

---
+++ js/owl-carousel.js
@@ -0,0 +1,17 @@
+$('#service-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
